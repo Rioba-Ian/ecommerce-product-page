3c59af961d1c9f8256cc1f36fcca79bc1948cf0b
Rioba-Ian <riobafelix@gmail.com>
2025-01-11 18:22:06 +0300

    add stripe and stripe checkout url

--- src/components/organisms/checkout/CheckoutButton.tsx
+++ src/components/organisms/checkout/CheckoutButton.tsx
@@ -1,6 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { stripePreorderLink } from "@/lib/stripe";
+import Link from "next/link";
 import React from "react";
 
 export default function CheckoutButton() {
- return <Button className="bg-primaryEcommerce w-1/2 mx-auto">Checkout</Button>;
+ return (
+  <Button className="bg-primaryEcommerce w-1/2 mx-auto" asChild>
+   <Link href={stripePreorderLink}>Checkout</Link>
+  </Button>
+ );
 }
---
+++ src/lib/stripe.ts
@@ -0,0 +1,2 @@
+export const stripePreorderLink =
+ "https://buy.stripe.com/test_8wM7urcMh76a3pC9AA";
